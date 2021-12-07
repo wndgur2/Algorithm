5671c4bcf104e2fe53bf29d17863daecbda33da0
leejunghyeok <wndgur76@naver.com>
2021-12-07 18:36:44 +0900

    12.7

--- c/mergeSort.c
+++ c/mergeSort.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 void merge(int arr[], int l, int m, int r){
 	int i, j, k;
 	int n1 = m - l + 1;
 	int n2 = r - m;
 
-	int L[n1*2], R[n2*2];
+	int L[n1], R[n2];
 
 	for (i = 0; i < n1; i++)
 		L[i] = arr[(l + i)];
