b00b7060b2e5d94e8084caf1443bdd79120dfffb
leejunghyeok <wndgur76@naver.com>
2022-02-20 16:40:31 +0900

    2.20

--- python/1753-최단경로.py
+++ python/1753-최단경로.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 
 V, E = map(int, sys.stdin.readline().split())
 S = int(sys.stdin.readline())
@@ -9,16 +10,17 @@ d = [3000001 for _ in range(V)]
 def bfs(start):
   #self에서 모두에게 가는 최단경로.
   d[start] = 0
-  q = [[start, 0]]
-  for node, l in q:
+  q = [[0, start]]
+  while q:
+    l, node = heapq.heappop(q)
     if(d[node] < l):
       continue
     for k, v in arrows[node]:
       if(d[k] > l+v):
-        q.append([k, l+v])
+        heapq.heappush(q, [l+v, k])
         d[k] = l+v
 
-for i in range(E):  
+for i in range(E):
   a, b, v = map(int, sys.stdin.readline().split())
   arrows[a-1].append([b-1, v])
 
